b8e67589141b42045fead37994609eb5e831a4f7
pablozerp <130076130+pablozerp@users.noreply.github.com>
2023-06-02 23:42:51 -0400

    creacion de la api de geolocalizacion

--- routes/index.js
+++ routes/index.js
@@ -2,23 +2,97 @@ var express = require('express');
 const db = require('../database');
 var router = express.Router();
 
+const fetch = require('node-fetch');
+const nodemailer = require('nodemailer');
+require('dotenv').config()
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Pablo Zerpa,31 786 816, seccion 3' });
 });
 
 
-router.post('/', function(req, res, next) {
-  let name = req.body.name;
-  let email = req.body.email;
+router.post('/', async function(req, res, next) {
+  let  name = req.body.name;
+  let  email = req.body.email;
   let comment = req.body.comment;
   let date = Date();
-  let ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  let country;
+  try {
+    const response = await fetch('https://api.ipify.org/?format=json');
+    const data = await response.json();
+    ip = data.ip;
+    console.log('La dirección IP del usuario es: ' + ip);
 
-  db.insert(name, email, comment, date, ip);
+    const url = 'http://api.ipstack.com/' + ip + '?access_key=470211dbb6394999a95614fd5799d524';
+    const response2 = await fetch(url);
+    const data2 = await response2.json();
+    country = data2.country_name;
+    console.log('El país del usuario es: ' + country);
 
-  res.redirect('/');
-});
+
+    enviarMail = async () => {
+      const config = {
+          host : 'smtp.gmail.com',
+          port : 587,
+          auth : {
+              user : 'm4g1c4l033@gmail.com',
+              pass : 'ipwuttxclkvqexeb'
+          }
+      }
+  
+      const mensaje = {
+          from : 'm4g1c4l033@gmail.com',
+          to : 'm4g1c4l033@gmail.com',
+          subject : 'correo de pruebas',
+          text : ' nombre: ' + name + ' comentario: ' + comment + ' email: ' + email + ' fecha: ' + date + ' la ip: ' + ip + ' el pais es: ' + country
+      }
+      const transport = nodemailer.createTransport(config);
+      const info = await transport.sendMail(mensaje);
+      console.log(info);
+  } 
+  
+  enviarMail();
+
+  
+
+
+
+
+
+
+    db.insert(name, email, comment, date, ip, country);
+    res.redirect('/');
+  } catch (error) {
+    console.error(error);
+  }
+})
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 router.get('/contactos', function(req, res, next) {
