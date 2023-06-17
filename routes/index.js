b983ede300abd74fd369e672a18b9f2f1373688f
pablozerp <130076130+pablozerp@users.noreply.github.com>
2023-06-17 19:33:14 -0400

    creacion de la vista contactos

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-  
+let ejs = require('ejs'); 
  var express = require('express');
 const db = require('../database');
 var router = express.Router();
@@ -24,11 +24,11 @@ router.post('/', async function(req, res, next) {
   let  email = req.body.email;
   let comment = req.body.comment;
   let date = Date();
-  let country;
-  let ip = req.headers['x-forwarded-for'] ||  req.socket.remoteAddress;
-  const myIP = ip.split(",")[0];
+  let country = "2";
+  let ip = "w" //req.headers['x-forwarded-for'] ||  req.socket.remoteAddress;
+  //const myIP = ip.split(",")[0];
   try {
-    const url = 'http://api.ipstack.com/' + myIP + '?access_key=470211dbb6394999a95614fd5799d524';
+    /*const url = 'http://api.ipstack.com/' + myIP + '?access_key=470211dbb6394999a95614fd5799d524';
     const response2 = await fetch(url);
     const data2 = await response2.json();
     country = data2.country_name;
@@ -51,7 +51,7 @@ router.post('/', async function(req, res, next) {
           from : process.env.USER,
           to : process.env.TO,
           subject : 'formulario programacion2',
-          text : ' nombre: ' + name + ' comentario: ' + comment + ' email: ' + email + ' fecha: ' + date + ' la ip: ' + myIP + ' el pais es: ' + country
+          text : ' nombre: ' + name + ' comentario: ' + comment + ' email: ' + email + ' fecha: ' + date + ' la ip: ' + ip + ' el pais es: ' + country
       }
       const transport = nodemailer.createTransport(config);
       const info = await transport.sendMail(mensaje);
@@ -59,9 +59,9 @@ router.post('/', async function(req, res, next) {
       console.log(info);
   } 
   
-  emailSubmit();
+  emailSubmit();*/
 
-  db.insert(name, email, comment, date, myIP, country);
+  db.insert(name, email, comment, date, ip, country);
     res.redirect('/');
   } catch (error) {
     console.error(error);
@@ -70,10 +70,16 @@ router.post('/', async function(req, res, next) {
 
 router.get('/contactos', function(req, res, next) {
   db.select(function (rows) {
+    res.render('contactos', { title: 'Registros del formulario', rows: rows,myToken:myToken,viss:viss });
+  });
+});
+/*
+router.get('/contactos', function(req, res, next) {
+  db.select(function (rows) {
     console.log(rows);
   });
   res.send('ok');
-});
+});*/
 
 
 module.exports = router;
